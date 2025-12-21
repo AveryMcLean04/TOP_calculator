8d4762dd32fac5010f22775e9b943424f7e325f4
Avery McLean <mcleanavery04@gmail.com>
2025-12-21 14:43:52 -0500

    Bulletproofed most of the input.

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function divide(x, y) {
     return x / y;
 }
 
+let hasDigit = false;
+let hasOperator = false;
+
 const digits = document.querySelectorAll('.digit');
 
 digits.forEach((button) => {
@@ -21,6 +24,7 @@ digits.forEach((button) => {
         const value = button.textContent;
         const resultDiv = document.querySelector('.result');
         resultDiv.textContent += value;
+        hasDigit = true;
     });
 })
 
@@ -29,29 +33,45 @@ const operators = document.querySelectorAll('.operator');
 
 operators.forEach((button) => {
     button.addEventListener('click', () => {
-        const value = button.textContent;
-        const resultDiv = document.querySelector('.result');
-        resultDiv.textContent += ` ${value} `;
+        if (!hasOperator && hasDigit) {
+            const value = button.textContent;
+            const resultDiv = document.querySelector('.result');
+            resultDiv.textContent += ` ${value} `;
+            hasOperator = true;
+        }
     });
 })
 
 const equalsButton = document.querySelector('.equals');
 
 equalsButton.addEventListener('click', () => {
-    const resultDiv = document.querySelector('.result');
-    let n = resultDiv.textContent.length;
-    const parts = resultDiv.textContent.split(' ');
-    const firstNumber = parseInt(parts[0]);
-    const lastNumber = parseInt(parts[2]);
+    let resultDiv = document.querySelector('.result');
+    let parts = resultDiv.textContent.split(' ');
+
+    let firstNumber = parseInt(parts[0]);
+    let lastNumber = parseInt(parts[2]);
     const operator = parts[1];
 
-    if (operator === '+') {
-        resultDiv.textContent = add(firstNumber, lastNumber);
-    } else if (operator === '-') {
-        resultDiv.textContent = subtract(firstNumber, lastNumber);
-    } else if (operator === 'x') {
-        resultDiv.textContent = multiply(firstNumber, lastNumber);
-    } else if (operator === '/') {
-        resultDiv.textContent = divide(firstNumber, lastNumber);
+    if (!isNaN(lastNumber)) {
+        if (operator === '+') {
+            resultDiv.textContent = add(firstNumber, lastNumber);
+        } else if (operator === '-') {
+            resultDiv.textContent = subtract(firstNumber, lastNumber);
+        } else if (operator === 'x') {
+            resultDiv.textContent = multiply(firstNumber, lastNumber);
+        } else if (operator === '/') {
+            resultDiv.textContent = divide(firstNumber, lastNumber);
+        }
     }
+    hasOperator = false;
+
+});
+
+const clearButton = document.querySelector('.clear');
+
+clearButton.addEventListener('click', () => {
+    const resultDiv = document.querySelector('.result');
+    resultDiv.textContent = '';
+    hasDigit = false;
+    hasOperator = false;
 });
